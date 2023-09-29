e9706f352e4268fef9ca54148aec2936fb971e22
Leandro Vilas Boas <leandrovboas@msn.com>
2023-09-29 14:25:45 -0300

    add git actions

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,22 +1,8 @@
-// const theme = {
-//   background: '#11172B',
-//   backgroundLight: '#1B2138',
-//   primary: '#0EE7B7',
-//   secondary: '#7AC7E3',
-//   gradient: 'linear-gradient(225deg, #313860 0%, #11172B 100%);',
-//   text: '#848DA0',
-//   textHighlight: '#444B5B',
-//   textLight: '#C4C4C4',
-//   border: '#313958',
-//   inputBackground: '#1E253E',
-//   error: '#d45d5d'
-// };
-
 const theme = {
-  background: '#3A3A3A',
+  background: '#1F232E',
   backgroundLight: '#6b6b70',
   backgroundDark: '#231f20',
-  primary: '#00c1c5',
+  primary: '#01ECFF',
   secondary: '#027F9F ',
   tertiary: '#B6E5EB',
   gradient: 'linear-gradient(225deg, #313860 0%, #11172B 100%);',
@@ -40,6 +26,11 @@ const theme = {
 // #1ED5A9
 // #B6E5EB
 // #ffffff
+
+//old
+// #3A3A3A
+// #00c1c5
+
 
 // http://ali-rifai.com/index.html
 
